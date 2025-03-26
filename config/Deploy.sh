e81b2ff19c2b7b4f9de509ab179c403cffbfa4a8
Chakib lemkhantar <Chakib.lemkhantar@nbs-consulting.net>
2025-03-26 11:28:09 +0000

    Initial Commit Config Files
    Config Files
    
    Config Files
    
    Update README.md

---
+++ config/Deploy.sh
@@ -0,0 +1,20 @@
+clear
+
+until read -r -p "Your Commit SHA: " commitSHA && test "$commitSHA" != ""; do
+  continue
+done
+
+until read -r -p "Your Destination Org : " destOrg && test "$destOrg" != ""; do
+  continue
+done
+
+
+echo "Deploy commit $commitSHA."
+echo "Modified Files to deploy to $destOrg : "
+git diff-tree --no-commit-id --name-only -r "$commitSHA"
+echo "+++++++++++++"
+
+files=$(git diff-tree --no-commit-id --name-only --diff-filter=AM -r $commitSHA | tr '\n' ',' | sed 's/,$//')
+
+echo "$files"
+sf force source deploy -p "$files" -u "$destOrg"
